04d504bfba4576efca34a2835dbd9a46a07e9f24
Jeffrey <jeff.isabella@gmail.com>
2021-12-26 18:47:47 -0500

    cleanup
    cleanup

--- app/models/behaviors/Factorial.scala
+++ app/models/behaviors/Factorial.scala
@@ -6,7 +6,7 @@ trait Factorial {
   // n! = n × (n−1)!
   @tailrec
   final def factorial(value: BigInt, acc: BigInt = BigInt(1)): BigInt = value match {
-    // TODO: what is the best way for us to prevent negative arguments?
+    case i if (i < 0) => throw new IllegalArgumentException(s"Cannot determine factorial of a negative number: [$i]")
     case i if (i == BigInt(0)) => acc
     case i => factorial(i - 1, acc * i)
   }
--- app/models/behaviors/pascal/PascalsTriangleByFactorial.scala
+++ app/models/behaviors/pascal/PascalsTriangleByFactorial.scala
@@ -6,7 +6,9 @@ trait PascalsTriangleByFactorial extends PascalsTriangle {
   type _Factorial <: Factorial
   val factorial: _Factorial
 
-  // n!/(n-r)! × r!
+  //       n!  
+  //    --------
+  //    (n-r)!r!
   override def pascalTriangle(rows: Int): Seq[Seq[BigInt]] = {
     for (i <- 0 until rows) yield {
       for (j <- 0 to i) yield {
