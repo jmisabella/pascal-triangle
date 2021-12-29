abdc2499b3e09ef52e583813af0fbbae9bb197f2
Jeffrey <jeff.isabella@gmail.com>
2021-12-29 18:36:00 -0500

    recent POC changes
    recent POC changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,99 +3,83 @@ var messageInput;
 
 $(document).on('mousemove', function(e){
   $("#modal").css({
-     left:  e.pageX - 200,
-     top:   e.pageY,
+     left:  e.pageX - 7,
+     top:   e.pageY - 30,
      show: false 
   });
 });
 
 $(document).ready(function() {
-  // $(".triangle-row span").hover(function() {
-  //   alert('hello');
-  //   return false;
-  // });
-
-  $(".triangle-row span").hover(function(e) {
-
+  $(".triangle-row span").mouseenter(function() {
     var clazz = $(this).attr("class");
     $("#modal-content").html(clazz);
-    
-    window.$('#modal').modal(function() {
-      // $( this ).fadeOut( 100 );
-      // $( this ).fadeIn( 500 );
-      show: true
-      show: false
-    });
-
-    // jQuery('#modal').modal({
-    //   show: true
-    // });
-
-    // $('#modal').modal({
-    //   show: true
-    // });
+    $("#modal").fadeIn(500);
+  });
+  $(".triangle-row span").mouseleave(function() {
+    $("#modal-content").html("");
+    $("#modal").fadeOut(100);
   });
 });
 
 function init() {
-    webSocket = new WebSocket("ws://localhost:9000/ws");
-    webSocket.onopen = onOpen;
-    webSocket.onclose = onClose;
-    webSocket.onmessage = onMessage;
-    webSocket.onerror = onError;
-    $("#message-input").focus();
+  webSocket = new WebSocket("ws://localhost:9000/ws");
+  webSocket.onopen = onOpen;
+  webSocket.onclose = onClose;
+  webSocket.onmessage = onMessage;
+  webSocket.onerror = onError;
+  $("#message-input").focus();
 }
 
 function onOpen(event) {
-    consoleLog("CONNECTED");
+  consoleLog("CONNECTED");
 }
 
 function onClose(event) {
-    consoleLog("DISCONNECTED");
-    appendClientMessageToView(":", "DISCONNECTED");
+  consoleLog("DISCONNECTED");
+  appendClientMessageToView(":", "DISCONNECTED");
 }
 
 function onError(event) {
-    consoleLog("ERROR: " + event.data);
-    consoleLog("ERROR: " + JSON.stringify(event));
+  consoleLog("ERROR: " + event.data);
+  consoleLog("ERROR: " + JSON.stringify(event));
 }
 
 function onMessage(event) {
-    console.log(event.data);
-    let receivedData = JSON.parse(event.data);
-    console.log("New Data: ", receivedData);
-    // get the text from the "body" field of the json we
-    // receive from the server.
-    appendServerMessageToView("Server", receivedData.body);
+  console.log(event.data);
+  let receivedData = JSON.parse(event.data);
+  console.log("New Data: ", receivedData);
+  // get the text from the "body" field of the json we
+  // receive from the server.
+  appendServerMessageToView("Server", receivedData.body);
 }
 
 function appendClientMessageToView(title, message) {
-    $("#message-content").append("<span>" + title + ": " + message + "<br /></span>");
+  $("#message-content").append("<span>" + title + ": " + message + "<br /></span>");
 }
 
 function appendServerMessageToView(title, message) {
-    $("#message-content").append("<span>" + title + ": " + message + "<br /><br /></span>");
+  $("#message-content").append("<span>" + title + ": " + message + "<br /><br /></span>");
 }
 
 function consoleLog(message) {
-    console.log("New message: ", message);
+  console.log("New message: ", message);
 }
 
 window.addEventListener("load", init, false);
 
 $("#send-button").click(function (e) {
-    console.log("Sending ...");
-    getMessageAndSendToServer();
-    // put focus back in the textarea
-    $("#message-input").focus();
+  console.log("Sending ...");
+  getMessageAndSendToServer();
+  // put focus back in the textarea
+  $("#message-input").focus();
 });
 
 // send the message when the user presses the <enter> key while in the textarea
 $(window).on("keydown", function (e) {
-    if (e.which == 13) {
-        getMessageAndSendToServer();
-        return false;
-    }
+  if (e.which == 13) {
+    getMessageAndSendToServer();
+    return false;
+  }
 });
 
 // there’s a lot going on here:
@@ -105,36 +89,35 @@ $(window).on("keydown", function (e) {
 // 4. send the message to the server.
 function getMessageAndSendToServer() {
 
-    // get the text from the textarea
-    messageInput = $("#message-input").val();
+  // get the text from the textarea
+  messageInput = $("#message-input").val();
 
-    // clear the textarea
-    $("#message-input").val("");
+  // clear the textarea
+  $("#message-input").val("");
 
-    // if the trimmed message was blank, return now
-    if ($.trim(messageInput) == "") {
-        return false;
-    }
+  // if the trimmed message was blank, return now
+  if ($.trim(messageInput) == "") {
+    return false;
+  }
 
-    // add the message to the view/div
-    appendClientMessageToView("Me", messageInput);
+  // add the message to the view/div
+  appendClientMessageToView("Me", messageInput);
 
-    // create the message as json
-    let jsonMessage = {
-        message: messageInput
-    };
+  // create the message as json
+  let jsonMessage = {
+    message: messageInput
+  };
 
-    // send our json message to the server
-    sendToServer(jsonMessage);
+  // send our json message to the server
+  sendToServer(jsonMessage);
 }
 
 // send the data to the server using the WebSocket
 function sendToServer(jsonMessage) {
-    if(webSocket.readyState == WebSocket.OPEN) {
-        consoleLog("SENT: " + jsonMessage.message);
-        webSocket.send(JSON.stringify(jsonMessage));
-    } else {
-        consoleLog("Could not send data. Websocket is not open.");
-    }
+  if(webSocket.readyState == WebSocket.OPEN) {
+    consoleLog("SENT: " + jsonMessage.message);
+    webSocket.send(JSON.stringify(jsonMessage));
+  } else {
+    consoleLog("Could not send data. Websocket is not open.");
+  }
 }
-
